58a187446a39d448f4409edbe6149d4c99625fe5
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-12 15:17:24 +0300

    Add project files.

---
+++ 01. Unique PIN Codes/Program.cs
@@ -0,0 +1,20 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num1; i++)
+{
+    for (int j = 1; j <= num2; j++)
+    {
+        for (int k = 1; k <= num3; k++)
+        {
+            if (i % 2 == 0 && k % 2 == 0)
+            {
+                if (j == 2 || j == 3 || j == 5 || j == 7)
+                {
+                    Console.WriteLine($"{i} {j} {k}");
+                }
+            }
+        }
+    }
+}
