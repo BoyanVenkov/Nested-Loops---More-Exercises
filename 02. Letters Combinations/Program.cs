d3b7789066559dccc0f4308448f153d5e62ac5ce
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-12 15:38:55 +0300

    Added a New Project

---
+++ 02. Letters Combinations/Program.cs
@@ -0,0 +1,20 @@
+﻿char letterOne = char.Parse(Console.ReadLine());
+char letterTwo = char.Parse(Console.ReadLine());
+char exception = char.Parse(Console.ReadLine());
+int combination = 0;
+string output = string.Empty;
+for(char i = letterOne; i <= letterTwo; i++)
+{
+    for (char j = letterOne; j <= letterTwo; j++)
+    {
+        for (char k = letterOne; k <= letterTwo; k++)
+        {
+            if (i != exception && j != exception && k != exception)
+            {
+                output += ($"{i}{j}{k} ");
+                combination++;
+            }
+        }
+    }
+}
+Console.WriteLine($"{output}{combination}");
