1b7f29855f5ca3eda1a761bfd02cd02077b6442c
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-12 16:01:08 +0300

    Added a New Project

---
+++ 05. Challenge the Wedding/Program.cs
@@ -0,0 +1,18 @@
+﻿int man = int.Parse(Console.ReadLine());
+int woman = int.Parse(Console.ReadLine());
+int table = int.Parse(Console.ReadLine());
+int combinations = 0;
+
+
+for (int i = 1; i <= man; i++)
+{
+    for(int j = 1; j <= woman; j++)
+    {
+        if (combinations >= table)
+        {
+            return;
+        }
+        Console.Write($"({i} <-> {j}) ");
+        combinations++;
+    }
+}
