e7c8a64b3ed7636ba7a6675a77756d8f2a62e41b
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-14 20:38:51 +0300

    Added a New Project

---
+++ 09. Sum of Two Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+int magicNumber = int.Parse(Console.ReadLine());
+int combinations = 0;
+int successfulCombinations = 0;
+for (int i = start; i <= end; i++)
+{
+    for (int j = start; j <= end; j ++)
+    {
+        combinations++;
+        if (i + j == magicNumber && successfulCombinations == 0)
+        {
+            Console.WriteLine($"Combination N:{combinations} ({i} + {j} = {magicNumber})");
+            successfulCombinations++;
+        }
+    }
+}
+if (successfulCombinations == 0)
+{
+    Console.WriteLine($"{combinations} combinations - neither equals {magicNumber}");
+}
