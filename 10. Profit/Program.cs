7349aaafaf25322b7d301f44b57f08abc3f12ba0
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-14 20:47:39 +0300

    Added a New Project

---
+++ 10. Profit/Program.cs
@@ -0,0 +1,18 @@
+﻿int oneLev = int.Parse(Console.ReadLine());
+int twoLev = int.Parse(Console.ReadLine());
+int fiveLev = int.Parse(Console.ReadLine());
+int sum = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= oneLev; i++)
+{
+    for (int j = 0; j <= twoLev; j++)
+    {
+        for (int k = 0; k <= fiveLev; k++)
+        {
+            if (i * 1 + j * 2 + k * 5 == sum)
+            {
+                Console.WriteLine($"{i} * 1 lv. + {j} * 2 lv. + {k} * 5 lv. = {sum} lv.");
+            }
+        }
+    }
+}
