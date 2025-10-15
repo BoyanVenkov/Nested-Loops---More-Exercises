3c71208b06a7a3e5efc186b3b345b044ee6a7a6c
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-15 19:12:24 +0300

    Added a New Project

---
+++ 12. The song of the wheels/Program.cs
@@ -0,0 +1,35 @@
+﻿int input = int.Parse(Console.ReadLine());
+int count = 0;
+string password = " ";
+
+for (int i = 1; i <= 9; i++)
+{
+    for (int j = 1; j <= 9; j++)
+    {
+        for (int k = 1; k <= 9; k++)
+        {
+            for (int l = 1; l <= 9; l++)
+            {
+                if (i < j && k > l && (i * j) + (k * l) == input)
+                {
+                    Console.Write($"{i}{j}{k}{l} ");
+                    count++;
+                    if (count == 4)
+                    {
+                        password = ($"{i}{j}{k}{l}");
+                    }
+                }
+            }
+        }
+    }
+}
+if (count < 4)
+{
+    Console.WriteLine();
+    Console.WriteLine("No!");
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine($"Password: {password}");
+}
