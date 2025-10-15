53d01fb35dd99016f60113ae4ed64ff3ca585e89
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-15 19:33:46 +0300

    Added a New Project

---
+++ 13. Prime Pairs/Program.cs
@@ -0,0 +1,31 @@
+﻿int firstStart = int.Parse(Console.ReadLine());
+int secondStart = int.Parse(Console.ReadLine());
+int firstDiff = int.Parse(Console.ReadLine());
+int secondDiff = int.Parse(Console.ReadLine());
+
+int firstEnd = firstStart + firstDiff;
+int secondEnd = secondStart + secondDiff;
+
+// Функция за проверка на просто число
+bool IsPrime(int number)
+{
+    if (number < 2) return false;
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
+}
+
+for (int first = firstStart; first <= firstEnd; first++)
+{
+    if (!IsPrime(first)) continue;
+
+    for (int second = secondStart; second <= secondEnd; second++)
+    {
+        if (!IsPrime(second)) continue;
+
+        Console.WriteLine($"{first}{second}");
+    }
+}
